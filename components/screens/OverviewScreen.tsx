e2da4584de3e92cda4a8e7ebe0183cd5b16a3465
Miguelin04 <miguel.a.luna@unl.edu.ec>
2026-01-22 08:25:03 -0500

    Fix: Corregir ruta de imagen en OverviewScreen

--- components/screens/OverviewScreen.tsx
+++ components/screens/OverviewScreen.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { PROBLEMS } from '../../constants';
-import profileImage from '../../img/imagen fondo .jpeg';
+import profileImage from '../../img/fondo.jpg.jpeg';
 
 interface OverviewScreenProps {
   onNext: () => void;
